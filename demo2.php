621ca730fc28659898a0df838919c7a4ce0bd63b
DonnieKing <974309696@qq.com>
2018-12-16 21:33:25 +0800

    测试数据库框架：Medoo

--- demo2.php
+++ demo2.php
@@ -9,3 +9,17 @@
  * 2、配置一下Medoo
  */
 
+//使用Composer自动加载器
+require __DIR__.'/vendor/autoload.php';
+
+//导入配置文件
+$config = require __DIR__.'/pig/config.php';
+
+//导入Medoo命名空间
+use Medoo\Medoo;
+
+$db = new Medoo($config['db']);
+
+$rows = $db->select('user',['id','name','dept','art','email']);
+
+echo '<pre>'.print_r($rows,true);
