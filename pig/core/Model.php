96e3763176bad9ed593432dbb2ac5a70cd84a3ba
DonnieKing <974309696@qq.com>
2018-12-19 22:34:21 +0800

    框架的模型基类

--- pig/core/Model.php
+++ pig/core/Model.php
@@ -7,8 +7,18 @@
 
 namespace pig\core;
 
+//引入Medoo框架类的命名空间
+use Medoo\Medoo;
 
-class Model
+class Model extends Medoo
 {
+    public function __construct($options=null)
+    {
+        //导入配置文件
+        $config = require __DIR__.'/../config.php';
+        $options = $config['db'];
 
+        //实例化数据库框架Medoo
+        parent::__construct($options);
+    }
 }
